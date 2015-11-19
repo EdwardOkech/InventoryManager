c01961d2d20f4166b347084ae64078aac3da3382
EdwardOkech <okechjobs@gmail.com>
2015-11-19 06:13:32 +0300

    Added the client folder

---
+++ client/templates/index.js
@@ -0,0 +1,40 @@
+/**
+ * Created by okech on 11/19/15.
+ */
+
+Template.home.helpers({
+    productsList: function(){
+        return [
+            {
+                item: 'Aucma Tv 32" ',
+                description : 'lorem ipsum borem sodum color sit amet',
+                serial_no: '#34567',
+                purchase_date: '10-11-2015',
+                quantity: '25',
+                unit_cost: 'Kes 20,900',
+                image :'this image'
+
+            },
+            {
+                item: 'DVD Aucma" ',
+                description : 'lorem ipsum borem sodum color sit amet',
+                serial_no: '#34569',
+                purchase_date: '10-11-2015',
+                quantity: '25',
+                unit_cost: 'Kes 3,999',
+                image :'this image'
+
+            },
+            {
+                item: 'Shoes Nike" ',
+                description : 'lorem ipsum borem sodum color sit amet',
+                serial_no: '#34566',
+                purchase_date: '10-11-2015',
+                quantity: '25',
+                unit_cost: 'Kes 20,900',
+                image :'this image'
+
+            },
+        ]
+    }
+})
